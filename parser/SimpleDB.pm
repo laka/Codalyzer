f11026a8020b5444b2d8493b4ccfe0a29828a4eb
laka <lakarlsen@gmail.com>
2009-02-17 22:13:38 +0000

    Wrote a quick DBI insert function

--- parser/SimpleDB.pm
+++ parser/SimpleDB.pm
@@ -1,6 +1,6 @@
-package OcSP::SimpleDB;
+package CA::SimpleDB;
 
-# OpenCodStats
+# Codalyzer
 # - Simple Database Functions
 # Just a wrapper for some common DBI-functions
 
@@ -8,34 +8,51 @@ use strict;
 use warnings;
 use DBI;
 use Carp;
-use lib '/home/homer/ju/jussimik/OCS-Parser/';
-use OcSP::Config;
+use lib '/home/homer/ju/jussimik/CA-Parser/';
+use CA::Config;
 
 BEGIN {
-	my %config = OcSP::Config::readConfig();
+	my %config = CA::Config::readConfig();
 	our $dbh = DBI->connect("dbi:mysql:" . "dbname=" .
 		$config{mysql_db} . ";host=" . $config{mysql_host},
         $config{mysql_user}, $config{mysql_pass})
-		or croak "OcSP (error): Couldn't connect to MySQL-database: " . DBI->errstr;
+		or croak "CA (error): Couldn't connect to MySQL-database: " . DBI->errstr;
 }
 
 our($dbh);
 
 sub getDbh {
-	my %config = OcSP::Config::readConfig();
+	my %config = CA::Config::readConfig();
 	if (!(defined($dbh) && $dbh->ping)) {
 		unless ($dbh = DBI->connect("dbi:mysql:" . "dbname=" .
 			$config{mysql_db} . ";host=" . $config{mysql_host},
 			$config{mysql_user}, $config{mysql_pass})) {
 				$dbh = undef;
-				croak "OcSP (error): Couldn't connect to MySQL-database: " . DBI->errstr;
+				croak "CA (error): Couldn't connect to MySQL-database: " . DBI->errstr;
 		}
 	}
 	return $dbh;
 }
 
+# subroutine: insertRow (table, %insert_hash)
+# -------------------------------------------------------------
+# Inserts the rows into the table specified.
+# NOTE: Uses placeholders, but should consider rewriting it 
+# using a prepare statement and quotes
+
+sub insertRow {
+    my $table = shift;
+    my %insert = @_;
+    my $placeholders = join(',', map { "?"; } (values(%insert)));
+
+    $dbh->do(
+        "INSERT INTO $table (".join(', ', keys %insert).")
+        VALUES ($placeholders)", undef, (values(%insert)))
+        or croak "OcSP (error): Couldn't insert row: " . DBI->errstr;
+}
+
 END {
-    our $dbh;
+	our $dbh;
     $dbh->disconnect;
 }
 
