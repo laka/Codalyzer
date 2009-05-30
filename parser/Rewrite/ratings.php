e3043c2970ea6ccce1339bc4f2b37310c8d24544
laka <lakarlsen@gmail.com>
2009-05-30 01:36:02 +0000

    while-loop misplaced.

--- parser/Rewrite/ratings.php
+++ parser/Rewrite/ratings.php
@@ -6,11 +6,8 @@ class rating extends toolbox {
 		$result = database::getInstance()->sqlResult(
 			"SELECT k_hash, c_hash FROM kills WHERE gid=\"$gid\" ORDER BY id");
 		
-		echo "GAME ID $gid\n";
 		$scores = array();
 		while($row = mysql_fetch_assoc($result)) {
-			
-			echo "\t$row[k_hash] - $row[c_hash]\n";
 			if(!isset($scores[$row['k_hash']])) {
 				$scores[$row['k_hash']] = $this->getPlayerElo($row['k_hash'], 'return');
 			}
@@ -21,15 +18,14 @@ class rating extends toolbox {
 			$change =  1/(1+pow(10,(($scores[$row['k_hash']] - $scores[$row['c_hash']])/400)));
 			
 			if($row['c_hash'] != $row['k_hash']) {
-				$scores{$row['k_hash']} += $change;
-				$scores{$row['c_hash']} -= $change;
+				$scores[$row['k_hash']] += $change;
+				$scores[$row['c_hash']] -= $change;
 			}
-			
-			while(list($key, $value) = each($scores)) {
-				#$value = $this->round($value, 2);
-				database::getInstance()->sqlResult(
-					"UPDATE players SET elo=\"$value\" WHERE hash=\"$key\" AND gid=\"$gid\"");
-			}
+		}
+		while(list($key, $value) = each($scores)) {
+			$value = round($value, 2);
+			database::getInstance()->sqlResult(
+				"UPDATE players SET elo=\"$value\" WHERE hash=\"$key\" AND gid=\"$gid\"");
 		}
 	}
 }
