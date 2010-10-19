aa73340a2a969b7102f5243bc921ab9b633b5eeb
laka <lakarlsen@gmail.com>
2010-10-19 17:53:05 +0000

    Added new structure for player handlings

---
+++ parser/Rewrite/2.0/players.php
@@ -0,0 +1,69 @@
+<?php
+
+class players {
+	public function addPlayer($matches, $gid) {
+		
+	}
+	
+	public function addTeam($matches, $gid) {
+	
+	}
+	
+	public function addAction($matches, $gid) {
+	
+	}
+	
+	public function addClanTag($matches, $gid) {
+	
+	}
+	
+	public function addQuote($matches, $gid) {
+	
+	}
+	
+	public function addNewAlias($alias, $owner, $gid) {
+		
+	}
+	
+	public function getEveryAlias($hash) {
+	
+	}
+	
+	public function makeProfile($hash, $handle) {
+	
+	}
+	
+	public function addProfileData($hash) {
+	
+	}
+	
+	public function sumKills($hash) {
+	
+	}
+	
+	public function sumDeaths($hash) {
+	
+	}
+	
+	public function sumSuicides($hash) {
+	
+	}
+	
+	public function sumGames($hash) {
+	
+	}
+	
+	public function playerELO($hash) {
+		
+	}
+	
+	public function getPlayerID($hash) {
+		
+	}
+	
+	public function playerInGame($handle) {
+	
+	}
+}
+
+?>
