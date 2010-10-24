e5cb9a1103b7ec99e1cad054d7b067698fbae169
laka <lakarlsen@gmail.com>
2010-10-24 17:05:22 +0000

    We are now adding players yet again

--- parser/Rewrite/2.0/players.php
+++ parser/Rewrite/2.0/players.php
@@ -2,11 +2,35 @@
 
 class players {
 	public function addPlayer($matches, $gid) {
+		$handle = $matches[3];
+		$ts = $this->inSeconds($matches[1]);
+		if(strlen($matches[2]) > 8) {
+			$hash = substr($matches[2], -8);
+		} else {
+			$this->newWarning('addPlayer', 'NO_HASH', $ts);
+		}
 		
+		$this->makeProfile($hash, $handle);
+		$playerID = $this->getPlayerID($hash);
+		
+		if(!$this->playerInGame($handle, $gid)) {
+			database::getInstance()->sqlResult("
+				INSERT INTO players (gid, ts, playerID, handle)
+				VALUES(\"$gid\", \"$ts\", \"$playerID\", \"$handle\")
+			");
+		}
 	}
 	
 	public function addTeam($matches, $gid) {
-	
+		$handle = $matches[1];
+		$team = $matches[2];
+
+		database::getInstance()->sqlResult("
+			UPDATE players 
+			SET team=\"$team\"
+			WHERE handle=\"$handle\"
+			AND gid=\"$gid\"
+		");
 	}
 	
 	public function addAction($matches, $gid) {
