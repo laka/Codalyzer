30756e09a654de982fbe83d7586b663e6c6b8f63
laka <lakarlsen@gmail.com>
2010-10-24 17:52:39 +0000

    Alias slayer!

--- parser/Rewrite/2.0/players.php
+++ parser/Rewrite/2.0/players.php
@@ -34,19 +34,80 @@ class players {
 	}
 	
 	public function addAction($matches, $gid) {
-	
+		$ts = $this->inSeconds($matches[1]);
+		$playerID = $this->getPlayerID($matches[3]);
+		$action = $matches[2];		
+
+		database::getInstance()->sqlResult("
+			INSERT INTO actions (gid, ts, action, playerID)
+			VALUES(\"$gid\", \"$ts\", \"$action\", \"$playerID\"
+		");	
 	}
 	
-	public function addClanTag($matches, $gid) {
-	
+	public function addClanTag($hash) {
+		$playerID = $this->getPlayerID($hash);
+		$handle = $this->getPlayerHandle($playerID);
+		
+		if(preg_match('/^\w+$/', $handle, $lone)) {
+			$clan = 'none';
+		} else {
+			if(preg_match('/(\w+).*?(\w+)$/', $handle, $match)) {
+				$clan = $match[1];
+			} else {
+				$clan = 'unknown';
+			}
+		}
+		database::getInstance()->sqlResult("
+			UPDATE profiles SET clan=\"$clan\" WHERE id=\"$playerID\"
+		");
 	}
 	
 	public function addQuote($matches, $gid) {
-	
+		$ts = $this->inSeconds($matches[1]);
+		$hash = substr($matches[2], -8);
+		$playerID = $this->getPlayerID($hash);
+		$quote = $matches[4];
+		
+		database::getInstance()->sqlResult("
+			INSERT INTO quotes (gid, ts, quote, playerID)
+			VALUES(\"$gid\", \"$ts\", \"$quote\", \"$playerID\")
+		");
 	}
 	
 	public function addNewAlias($alias, $owner, $gid) {
-		
+		if(strlen($alias) > 0) {
+			$alias = database::getInstance()->sqlQuote($alias);
+			$exist = database::getInstance()->sqlResult("
+				SELECT id
+				FROM alias
+				WHERE owner=\"$owner\"
+				AND alias=\"$alias\"
+			");
+			if(mysql_num_rows($exist) == 0) {
+				database::getInstance()->sqlResult("
+					INSERT INTO alias (owner, alias)
+					VALUES(\"$owner\", \"$alias\")
+				");
+			}
+			if(is_numeric($gid)) {
+				$row = database::getInstance()->singleRow("
+					SELECT handle 
+					AS old_handle 
+					FROM players 
+					WHERE hash=\"$owner\" 
+					AND gid=\"$gid\"
+				");
+				
+				database::getInstance()->sqlResult("
+					UPDATE players 
+					SET handle=\"$alias\" 
+					WHERE hash=\"$owner\" 
+					AND gid=\"$gid\"
+				");
+				
+				$this->addNewAlias($row[old_handle], $owner, '');
+			}
+		}		
 	}
 	
 	public function getEveryAlias($hash) {
